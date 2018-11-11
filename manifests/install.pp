568bd1dfd34506692d1f226b2fc9450a6e3ae112
belfast77 <belfast77@gmail.com>
2018-11-11 19:11:53 +0000

    removed yum repo for testing

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,20 +6,22 @@
 #   include liagent::install
 class liagent::install {
 
+#  include liagent::yum
+
   if $liagent::package_manage == true {
 
     if $liagent::version == undef {
 
       package { $liagent::package:
         ensure  => installed,
-        require => Yumrepo['$liagent::loginsight_repo'],
+#        require => Yumrepo['$liagent::loginsight_repo'],
       }
 
     } else {
 
       package { $liagent::package:
         ensure  => $liagent::version,
-        require => Yumrepo['$liagent::loginsight_repo'],
+#        require => Yumrepo['$liagent::loginsight_repo'],
       }
     }
   }
