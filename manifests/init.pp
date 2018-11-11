1a1d0ab2c164f6c47275573fb3ec9aa37880ec9e
belfast77 <belfast77@gmail.com>
2018-11-11 14:33:46 +0000

    adding defaults and lookups

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,19 +10,25 @@ class liagent (
   String $srv_hostname,
   String $proto,
   Optional[Integer[0, 65535]] $port,
-  Boolean[Stdlib::bool2str[true, 'yes', 'no'] $ssl
-  Optional[Stdlib::Absolutepath] $ssl_ca_path
-  Optional[Integer[15, 60]] $reconnect
-  Boolean[Stdlib::bool2str[true, 'yes', 'no'] $central_config
-  Optional[Integer[0, 2]] $debug_level
-  Optional[Integer[1, 3600]] $stats_period
-  Boolean[Stdlib::bool2str[true, 'yes', 'no'] $smart_stats
-  Optional[Integer[100, 2000]] $max_disk_buffer
-  String $filelog
-  String $syslog
-  Stdlib::Absolutepath $directory
-  String $include
-  Optional[Boolean[Stdlib::bool2str[true, 'yes', 'no']] $auto_update
+#  # Would like to restrict the option here but think it's my lack of
+#  # Understanding on how to use bool2str
+  String $ssl,
+#  Boolean[Stdlib::bool2str[true, 'yes', 'no'] $ssl,
+  Optional[Stdlib::Absolutepath] $ssl_ca_path,
+  Optional[Integer[15, 60]] $reconnect,
+  String $central_config,
+#  Boolean[Stdlib::bool2str[true, 'yes', 'no'] $central_config,
+  Optional[Integer[0, 2]] $debug_level,
+  Optional[Integer[1, 3600]] $stats_period,
+  String $smart_stats,
+#  Boolean[Stdlib::bool2str[true, 'yes', 'no']] $smart_stats,
+  Optional[Integer[100, 2000]] $max_disk_buffer,
+  String $filelog,
+  String $logtype,
+  Stdlib::Absolutepath $directory,
+  String $include,
+  String $auto_update,
+#  Optional[Boolean[Stdlib::bool2str[true, 'yes', 'no']]] $auto_update,
   Boolean $service_manage,
   Boolean $service_ensure,
   Boolean $service_enable,
@@ -31,8 +37,10 @@ class liagent (
   Boolean $package_manage,
   Array[String] $package,
   String $version,
-  string $loginsight_repo,
-) {
+  String $loginsight_repo,
+)
+
+  {
 
   contain liagent::install
   contain liagent::config
