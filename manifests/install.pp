2bcbf447e2d464e688dffd51046e5e288eda683b
belfast77 <belfast77@gmail.com>
2018-11-12 19:28:01 +0000

    fixing yum class

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,22 +6,20 @@
 #   include liagent::install
 class liagent::install {
 
-#  include liagent::yum
-
   if $liagent::package_manage == true {
 
     if $liagent::version == undef {
 
       package { $liagent::package:
         ensure  => installed,
-#        require => Yumrepo['$liagent::loginsight_repo'],
+        require => Yum['$liagent::loginsight_repo'],
       }
 
     } else {
 
       package { $liagent::package:
         ensure  => $liagent::version,
-#        require => Yumrepo['$liagent::loginsight_repo'],
+        require => Yum['$liagent::loginsight_repo'],
       }
     }
   }
